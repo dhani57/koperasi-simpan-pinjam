c31e842df51509ab22b88259a1b7ea86d4dd0bd1
dhni-mad <dhnahmad32@gmail.com>
2026-06-18 16:40:25 +0700

    feat: allow Vite development server domains in Content Security Policy headers

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -17,13 +17,16 @@ class ContentSecurityPolicy
     {
         $response = $next($request);
 
+        $viteDevServer = app()->environment('local') ? 'http://localhost:5173 ws://localhost:5173' : '';
+
         // Required headers from mandatory-secure-web-skills
-        $csp = "default-src 'self'; ";
+        $csp = "default-src 'self' $viteDevServer; ";
         // unsafe-inline and unsafe-eval are often needed for Vite/React dev server.
-        $csp .= "script-src 'self' 'unsafe-inline' 'unsafe-eval'; ";
-        $csp .= "style-src 'self' 'unsafe-inline' https://fonts.bunny.net https://fonts.googleapis.com; ";
+        $csp .= "script-src 'self' 'unsafe-inline' 'unsafe-eval' $viteDevServer; ";
+        $csp .= "style-src 'self' 'unsafe-inline' https://fonts.bunny.net https://fonts.googleapis.com $viteDevServer; ";
         $csp .= "font-src 'self' https://fonts.bunny.net https://fonts.gstatic.com; ";
         $csp .= "img-src 'self' data: https: http:; ";
+        $csp .= "connect-src 'self' $viteDevServer; ";
         $csp .= "frame-ancestors 'self'; ";
         $csp .= "object-src 'none';";
 
